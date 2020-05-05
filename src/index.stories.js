b2a142d61ac40e90cfc46ee37a5be4c2806ff44a
Manmohan Singh <manmohan.singh@publicissapient.com>
2020-05-05 09:46:32 +0530

    button text color changed

--- src/index.stories.js
+++ src/index.stories.js
@@ -3,7 +3,13 @@ import { Button } from '@storybook/react/demo';
 
 export default { title: 'Button' };
 
-export const withText = () => <Button>Hello Button</Button>;
+export const withText = () => (
+    <Button>
+        <span style={{color: "red"}}>
+            Hello Button
+        </span>
+    </Button>
+);
 
 export const withEmoji = () => (
   <Button>
